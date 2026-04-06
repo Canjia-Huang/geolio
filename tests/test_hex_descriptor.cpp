a7dfcfc40b9e6a4effb1778d03a7e2f45fac1cf3
Canjia Huang <huangcanjia0214@gmail.com>
2026-04-06 15:34:29 +0800

    feat: update HexDescriptorTest to use new mesh structure

--- tests/test_hex_descriptor.cpp
+++ tests/test_hex_descriptor.cpp
@@ -15,31 +15,32 @@ namespace GEO::MeshUtils::Test
     class HexDescriptorTest : public ::testing::Test {
     public:
         void SetUp() override {
-            M_.vertices.create_vertices(8);
-            M_.vertices.point(0) = GEO::vec3(0,0,0);
-            M_.vertices.point(1) = GEO::vec3(1,0,0);
-            M_.vertices.point(2) = GEO::vec3(0,1,0);
-            M_.vertices.point(3) = GEO::vec3(1,1,0);
-            M_.vertices.point(4) = GEO::vec3(0,0,1);
-            M_.vertices.point(5) = GEO::vec3(1,0,1);
-            M_.vertices.point(6) = GEO::vec3(0,1,1);
-            M_.vertices.point(7) = GEO::vec3(1,1,1);
-            M_.cells.create_hex(0,1,2,3,4,5,6,7);
+            M.vertices.create_vertices(8);
+            M.vertices.point(0) = GEO::vec3(0,0,0);
+            M.vertices.point(1) = GEO::vec3(1,0,0);
+            M.vertices.point(2) = GEO::vec3(0,1,0);
+            M.vertices.point(3) = GEO::vec3(1,1,0);
+            M.vertices.point(4) = GEO::vec3(0,0,1);
+            M.vertices.point(5) = GEO::vec3(1,0,1);
+            M.vertices.point(6) = GEO::vec3(0,1,1);
+            M.vertices.point(7) = GEO::vec3(1,1,1);
+            M.cells.create_hex(0,1,2,3,4,5,6,7);
         }
 
-        GEO::Mesh M_;
+        GEO::Mesh M;
+        const GEO::index_t c = 0;
     };
 
     TEST_F(HexDescriptorTest, HEX_LV_ADJACENT_LV) {
-        ASSERT_EQ(M_.cells.nb_vertices(0), HEX_LV_ADJACENT_LV.size());
+        ASSERT_EQ(M.cells.nb_vertices(c), HEX_LV_ADJACENT_LV.size());
 
-        for (GEO::index_t lv = 0; lv < M_.cells.nb_vertices(0); ++lv) {
-            const auto v = M_.cells.vertex(0, lv);
+        for (GEO::index_t lv = 0; lv < M.cells.nb_vertices(c); ++lv) {
+            const auto v = M.cells.vertex(c, lv);
 
             std::unordered_set<GEO::index_t> adj_vertices;
-            for (GEO::index_t le = 0; le < M_.cells.nb_edges(0); ++le) {
-                const auto& ev0 = M_.cells.edge_vertex(0, le, 0);
-                const auto& ev1 = M_.cells.edge_vertex(0, le, 1);
+            for (GEO::index_t le = 0; le < M.cells.nb_edges(c); ++le) {
+                const auto& ev0 = M.cells.edge_vertex(c, le, 0);
+                const auto& ev1 = M.cells.edge_vertex(c, le, 1);
                 if (ev0 == v)
                     adj_vertices.insert(ev1);
                 if (ev1 == v)
@@ -53,15 +54,15 @@ namespace GEO::MeshUtils::Test
     }
 
     TEST_F(HexDescriptorTest, HEX_LV_INCIDENT_LE) {
-        ASSERT_EQ(M_.cells.nb_vertices(0), HEX_LV_INCIDENT_LE.size());
+        ASSERT_EQ(M.cells.nb_vertices(c), HEX_LV_INCIDENT_LE.size());
 
-        for (GEO::index_t lv = 0; lv < M_.cells.nb_vertices(0); ++lv) {
-            const auto v = M_.cells.vertex(0, lv);
+        for (GEO::index_t lv = 0; lv < M.cells.nb_vertices(c); ++lv) {
+            const auto v = M.cells.vertex(c, lv);
 
             std::unordered_set<GEO::index_t> adj_edges;
-            for (GEO::index_t le = 0; le < M_.cells.nb_edges(0); ++le) {
-                const auto& ev0 = M_.cells.edge_vertex(0, le, 0);
-                const auto& ev1 = M_.cells.edge_vertex(0, le, 1);
+            for (GEO::index_t le = 0; le < M.cells.nb_edges(c); ++le) {
+                const auto& ev0 = M.cells.edge_vertex(c, le, 0);
+                const auto& ev1 = M.cells.edge_vertex(c, le, 1);
                 if (ev0 == v || ev1 == v)
                     adj_edges.insert(le);
             }
@@ -73,15 +74,15 @@ namespace GEO::MeshUtils::Test
     }
 
     TEST_F(HexDescriptorTest, HEX_LV_INCIDENT_LF) {
-        ASSERT_EQ(M_.cells.nb_vertices(0), HEX_LV_INCIDENT_LF.size());
+        ASSERT_EQ(M.cells.nb_vertices(c), HEX_LV_INCIDENT_LF.size());
 
-        for (GEO::index_t lv = 0; lv < M_.cells.nb_vertices(0); ++lv) {
-            const auto v = M_.cells.vertex(0, lv);
+        for (GEO::index_t lv = 0; lv < M.cells.nb_vertices(c); ++lv) {
+            const auto v = M.cells.vertex(c, lv);
 
             std::unordered_set<GEO::index_t> adj_facets;
-            for (GEO::index_t lf = 0; lf < M_.cells.nb_facets(0); ++lf) {
-                for (GEO::index_t lfv = 0; lfv < M_.cells.facet_nb_vertices(0, lf); ++lfv) {
-                    if (M_.cells.facet_vertex(0, lf, lfv) == v) {
+            for (GEO::index_t lf = 0; lf < M.cells.nb_facets(c); ++lf) {
+                for (GEO::index_t lfv = 0; lfv < M.cells.facet_nb_vertices(c, lf); ++lfv) {
+                    if (M.cells.facet_vertex(c, lf, lfv) == v) {
                         adj_facets.insert(lf);
                         break;
                     }
@@ -95,13 +96,13 @@ namespace GEO::MeshUtils::Test
     }
 
     TEST_F(HexDescriptorTest, HEX_LE_INCIDENT_LV) {
-        ASSERT_EQ(M_.cells.nb_edges(0), HEX_LE_INCIDENT_LV.size());
+        ASSERT_EQ(M.cells.nb_edges(c), HEX_LE_INCIDENT_LV.size());
 
-        for (GEO::index_t le = 0; le < M_.cells.nb_edges(0); ++le) {
+        for (GEO::index_t le = 0; le < M.cells.nb_edges(c); ++le) {
             std::unordered_set<GEO::index_t> adj_vertices;
 
             for (GEO::index_t i = 0; i < 2; ++i)
-                adj_vertices.insert(M_.cells.edge_vertex(0, le, i));
+                adj_vertices.insert(M.cells.edge_vertex(c, le, i));
 
             EXPECT_EQ(adj_vertices.size(), HEX_LE_INCIDENT_LV[le].size());
             for (const auto& adj_v : HEX_LE_INCIDENT_LV[le])
@@ -110,37 +111,37 @@ namespace GEO::MeshUtils::Test
     }
 
     TEST_F(HexDescriptorTest, HEX_ENCODED_LE) {
-        ASSERT_EQ(M_.cells.nb_edges(0), HEX_ENCODED_LE.size());
+        ASSERT_EQ(M.cells.nb_edges(c), HEX_ENCODED_LE.size());
 
-        for (GEO::index_t le = 0; le < M_.cells.nb_edges(0); ++le) {
-            const auto& ev0 = M_.cells.edge_vertex(0, le, 0);
-            const auto& ev1 = M_.cells.edge_vertex(0, le, 1);
+        for (GEO::index_t le = 0; le < M.cells.nb_edges(c); ++le) {
+            const auto& ev0 = M.cells.edge_vertex(c, le, 0);
+            const auto& ev1 = M.cells.edge_vertex(c, le, 1);
             EXPECT_EQ((1<<ev0) | (1<<ev1), HEX_ENCODED_LE[le]);
         }
     }
 
     TEST_F(HexDescriptorTest, HEX_LF_INCIDENT_LV) {
-        ASSERT_EQ(M_.cells.nb_facets(0), HEX_LF_INCIDENT_LV.size());
+        ASSERT_EQ(M.cells.nb_facets(0), HEX_LF_INCIDENT_LV.size());
 
-        for (GEO::index_t lf = 0; lf < M_.cells.nb_facets(0); ++lf) {
-            ASSERT_EQ(M_.cells.facet_nb_vertices(0, lf), HEX_LF_INCIDENT_LV[lf].size());
-            for (GEO::index_t lv = 0; lv < M_.cells.facet_nb_vertices(0, lf); ++lv)
-                EXPECT_EQ(M_.cells.facet_vertex(0, lf, lv), HEX_LF_INCIDENT_LV[lf][lv]);
+        for (GEO::index_t lf = 0; lf < M.cells.nb_facets(c); ++lf) {
+            ASSERT_EQ(M.cells.facet_nb_vertices(c, lf), HEX_LF_INCIDENT_LV[lf].size());
+            for (GEO::index_t lv = 0; lv < M.cells.facet_nb_vertices(c, lf); ++lv)
+                EXPECT_EQ(M.cells.facet_vertex(c, lf, lv), HEX_LF_INCIDENT_LV[lf][lv]);
         }
     }
 
     TEST_F(HexDescriptorTest, HEX_LF_INCIDENT_LE) {
-        ASSERT_EQ(M_.cells.nb_facets(0), HEX_LF_INCIDENT_LE.size());
+        ASSERT_EQ(M.cells.nb_facets(c), HEX_LF_INCIDENT_LE.size());
 
-        for (GEO::index_t lf = 0; lf < M_.cells.nb_facets(0); ++lf) {
+        for (GEO::index_t lf = 0; lf < M.cells.nb_facets(c); ++lf) {
             std::unordered_set<GEO::index_t> facet_vertices;
-            for (GEO::index_t lv = 0; lv < M_.cells.facet_nb_vertices(0, lf); ++lv)
-                facet_vertices.insert(M_.cells.facet_vertex(0, lf, lv));
+            for (GEO::index_t lv = 0; lv < M.cells.facet_nb_vertices(c, lf); ++lv)
+                facet_vertices.insert(M.cells.facet_vertex(c, lf, lv));
 
             std::unordered_set<GEO::index_t> adj_edges;
-            for (GEO::index_t le = 0; le < M_.cells.nb_edges(0); ++le) {
-                const auto& ev0 = M_.cells.edge_vertex(0, le, 0);
-                const auto& ev1 = M_.cells.edge_vertex(0, le, 1);
+            for (GEO::index_t le = 0; le < M.cells.nb_edges(c); ++le) {
+                const auto& ev0 = M.cells.edge_vertex(c, le, 0);
+                const auto& ev1 = M.cells.edge_vertex(c, le, 1);
                 if (facet_vertices.contains(ev0) && facet_vertices.contains(ev1))
                     adj_edges.insert(le);
             }
@@ -152,17 +153,17 @@ namespace GEO::MeshUtils::Test
     }
 
     TEST_F(HexDescriptorTest, HEX_LF_OPPOSITE_LF) {
-        ASSERT_EQ(M_.cells.nb_facets(0), HEX_LF_OPPOSITE_LF.size());
+        ASSERT_EQ(M.cells.nb_facets(c), HEX_LF_OPPOSITE_LF.size());
 
-        for (GEO::index_t lf = 0; lf < M_.cells.nb_facets(0); ++lf) {
+        for (GEO::index_t lf = 0; lf < M.cells.nb_facets(c); ++lf) {
             std::unordered_set<GEO::index_t> facet_vertices;
-            for (GEO::index_t lv = 0; lv < M_.cells.facet_nb_vertices(0, lf); ++lv)
-                facet_vertices.insert(M_.cells.facet_vertex(0, lf, lv));
+            for (GEO::index_t lv = 0; lv < M.cells.facet_nb_vertices(c, lf); ++lv)
+                facet_vertices.insert(M.cells.facet_vertex(c, lf, lv));
 
-            for (GEO::index_t nlf = 0; nlf < M_.cells.nb_facets(0); ++nlf) {
+            for (GEO::index_t nlf = 0; nlf < M.cells.nb_facets(c); ++nlf) {
                 bool opposite = true;
-                for (GEO::index_t nlv = 0; nlv < M_.cells.facet_nb_vertices(0, nlf); ++nlv) {
-                    if (facet_vertices.contains(M_.cells.facet_vertex(0, nlf, nlv))) {
+                for (GEO::index_t nlv = 0; nlv < M.cells.facet_nb_vertices(c, nlf); ++nlv) {
+                    if (facet_vertices.contains(M.cells.facet_vertex(c, nlf, nlv))) {
                         opposite = false;
                         break;
                     }
@@ -174,14 +175,14 @@ namespace GEO::MeshUtils::Test
     }
 
     TEST_F(HexDescriptorTest, HEX_ENCODED_LF) {
-        ASSERT_EQ(M_.cells.nb_facets(0), HEX_ENCODED_LF.size());
+        ASSERT_EQ(M.cells.nb_facets(c), HEX_ENCODED_LF.size());
 
-        for (GEO::index_t lf = 0; lf < M_.cells.nb_facets(0); ++lf) {
-            ASSERT_EQ(M_.cells.facet_nb_vertices(0, lf), 4);
-            const auto& ev0 = M_.cells.facet_vertex(0, lf, 0);
-            const auto& ev1 = M_.cells.facet_vertex(0, lf, 1);
-            const auto& ev2 = M_.cells.facet_vertex(0, lf, 2);
-            const auto& ev3 = M_.cells.facet_vertex(0, lf, 3);
+        for (GEO::index_t lf = 0; lf < M.cells.nb_facets(c); ++lf) {
+            ASSERT_EQ(M.cells.facet_nb_vertices(c, lf), 4);
+            const auto& ev0 = M.cells.facet_vertex(c, lf, 0);
+            const auto& ev1 = M.cells.facet_vertex(c, lf, 1);
+            const auto& ev2 = M.cells.facet_vertex(c, lf, 2);
+            const auto& ev3 = M.cells.facet_vertex(c, lf, 3);
             EXPECT_EQ((1<<ev0) | (1<<ev1) | (1<<ev2) | (1<<ev3), HEX_ENCODED_LF[lf]);
         }
     }
