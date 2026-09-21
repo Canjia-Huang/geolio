67dc1ccbf11550e00d76530cc6642f1488922b15
Canjia Huang <60030853+Canjia-Huang@users.noreply.github.com>
2026-09-21 21:52:05 +0800

    chore: reorder includes in test files for consistency

--- tests/mesh/test_hex_operations.cpp
+++ tests/mesh/test_hex_operations.cpp
@@ -2,14 +2,13 @@
 // Created by huangcanjia <huangcanjia0214@gmail.com> on 2026/5/2.
 // Copyright (c) 2026 Graphics@XMU (https://graphics.xmu.edu.cn). All rights reserved.
 //
-#include <unordered_set>
-#include <gtest/gtest.h>
-#include <geogram/mesh/mesh.h>
-#include <geolio/mesh/hex_operations.h>
 #include <bit>
 #include <ranges>
+#include <unordered_set>
+#include <geogram/mesh/mesh.h>
 #include <geogram/mesh/mesh_repair.h>
-
+#include <geolio/mesh/hex_operations.h>
+#include <gtest/gtest.h>
 #include "../utils.h"
 
 namespace geolio::test
--- tests/mesh/test_quad_operations.cpp
+++ tests/mesh/test_quad_operations.cpp
@@ -2,10 +2,12 @@
 // Created by huangcanjia <huangcanjia0214@gmail.com> on 2026/9/21.
 // Copyright (c) 2026 Graphics@XMU (https://graphics.xmu.edu.cn). All rights reserved.
 //
+#include <bit>
 #include <ranges>
+#include <unordered_set>
 #include <geogram/mesh/mesh_repair.h>
-#include <gtest/gtest.h>
 #include <geolio/mesh/quad_operations.h>
+#include <gtest/gtest.h>
 #include "../utils.h"
 
 namespace geolio::test
